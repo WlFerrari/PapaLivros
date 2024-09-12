b9b3838ef68460431fb7d422f6b81d0d56146f7f
Ferrari <vitorferrarijoao5@gmail.com>
2024-09-12 19:53:56 -0300

    Final Codes

---
+++ Script/script-V8.js
@@ -0,0 +1,229 @@
+
+function abrirpopup(title) {
+    const infos = 
+
+[
+  {
+    "nome": "A República",
+    "categoria": "Filosofia",
+    "imagem": "https://m.media-amazon.com/images/I/81KkahDyzgL._AC_UF1000,1000_QL80_.jpg",
+    "descricao": "A República é a obra mais importante de Platão filósofo grego que vivem entre os séculos V e IV antes de Cristo. De caráter político, prega um Estado ideal governado por líderes justos, sábios e instruídos. Para Platão, o homem capaz de reunir essas qualidades seria o filósofo. Ele mesmo teve a oportunidade de colocar em prática suas ideias, por três, todas fracassadas. Em A República Platão passa em revista todos os sistemas de governo existentes na época e analisa sobretudo a aristocracia, a oligarquia, a democracia e a ditadura. Ainda que reconheça que sua república é uma utopia, defende que esse seria o único caminho para uma sociedade perfeita. À parte alguns princípios sociais inaceitáveis hoje, tudo é de tamanha atualidade que até parece retratar os nossos dias, apesar de ter sido escrita há mais de 2.300 anos",
+    "link": "https://www.eniopadilha.com.br/documentos/Platao_A_Republica.pdf"
+    },
+    {
+        "nome": "Confissões",
+        "categoria": "Religião e Espirutualidade",
+        "imagem": "https://m.media-amazon.com/images/I/81wTnz5u8oL._AC_UF1000,1000_QL80_.jpg",
+        "descricao": "Redigido no século IV, entre Antiguidade e Idade Média, as Confissões de Agostinho de Hipona são ainda hoje um livro surpreendente. Por um lado, pela densidade poética e pela originalidade da escrita, e por inaugurar o gênero da autobiografia como história da formação de uma personalidade, elas representam um marco único na história da literatura ocidental. Por outro, Agostinho elabora nelas uma nova maneira de fazer filosofia, estranha à tradição antiga, por ser baseada não apenas em conceitos abstratos e deduções, mas sobretudo na observação fina dos movimentos psicológicos, das motivações interiores e do significado de pequenos fatos e gestos cotidianos. O resultado é uma leitura incontornável para todos os que se interessam por filosofia, história ou religião.",
+        "link":"https://img.cancaonova.com/noticias/pdf/277537_SantoAgostinho-Confissoes.pdf"
+      },
+    {
+        "nome": "Dom Casmurro",
+        "categoria": "Romance",
+        "imagem": "https://http2.mlstatic.com/D_NQ_NP_693460-MLU50419145414_062022-O.webp",
+        "descricao": "Em Dom Casmurro, o narrador Bento Santiago retoma a infância que passou na Rua de Matacavalos e conta a história do amor e das desventuras que viveu com Capitu, uma das personagens mais enigmáticas e intrigantes da literatura brasileira. Nas páginas deste romance, encontra-se a versão de um homem perturbado pelo ciúme, que revela aos poucos sua psicologia complexa e enreda o leitor em sua narrativa ambígua acerca do acontecimento ou não do adultério da mulher com olhos de ressaca, uma das maiores polêmicas da literatura brasileira.",
+        "link":"https://ddcus.org/pdf/summer_reading/11th_grade/Dom_Casmurro-Machado_de_Assis.pdf"
+      },
+    {
+        "nome": "O Banquete",
+        "categoria": "Filosofia",
+        "imagem": "https://www.baixelivros.com.br/media/2019/01/o-banquete.jpg",
+        "descricao": "Platão, ícone da filosofia ocidental, apresenta ao leitor de O Banquete uma das obras-mestras do clássico pensamento grego acerca do Amor (Eros).\nEm uma série de discursos que versam sobre a natureza e as qualidades desse tema, alvo das mais elevadas aspirações da alma humana, distintas facetas do amor são reveladas.\nPermeada pelo diálogo entre Apolodoro, discípulo e amigo de Sócrates, e Gláucon, uma personalidade obscura e alheia ao mundo da filosofia, emerge a narrativa dos sete discursos amorosos em elogio a Eros, o deus do amor, proferidos no banquete oferecido pelo poeta trágico ateniense Agaton em comemoração ao seu primeiro sucesso dramático.\nComo oradores, estiveram presentes:\n    Sócrates;\n    Aristodemo, amigo e discípulo de Sócrates;\n    Fedro;\n    Pausânias, o médico Erixímaco;\n    o comediante Aristófanes;\n    Alcibíades, um político. \nPausânias propõe que realizem um encontro diferente, cujo foco os distancie da embriaguez.\nErixímaco sugere que os convidados profiram discursos no intuito de louvar Eros, o deus do amor.\nDiscursos esses investidos de solenidade e reverência por parte dos oradores, à exceção de Alcibíades, cujo discurso teve por objeto o próprio Sócrates.\nNa presente obra nota-se pontos de grande divergência e inúmeros outros de confluência entre as concepções de amor advindas da sociedade grega clássica e as praticadas pela sociedade contemporânea, principalmente no que diz respeito aos mitos e às crenças amorosas ainda presentes na atualidade. \nO Banquete, que evidencia a conotação e a dimensão éticas do amor, constitui-se num vasto e deleitoso campo para a reflexão e a aprendizagem das origens que igualmente compuseram nossa cultura.\nUma brilhante obra, considerada um clássico da literatura filosófica ocidental!\nIndicada a todos aqueles que buscam refletir sobre o amor, suas origens e os mitos que o envolvem. \nO Banquete é um convite ao repensar as práticas da atualidade e aquelas enraizadas na clássica sociedade grega.",
+        "link": "https://criticanarede.com/docs/banquete.pdf"
+      },
+    {
+        "nome": "Poema Sujo",
+        "categoria": "Poesia",
+        "imagem": "https://www.bpp.pr.gov.br/sites/biblioteca/arquivos_restritos/files/migrados/Image/Candido_20/capapoemasujooriginal.jpg",
+        "descricao": "\"Poema Sujo\" nasceu num momento difícil da história do país e em circunstâncias dramáticas da vida de Ferreira Gullar. um dos maiores poetas brasileiros. então no exílio. Em 2006. este longo poema completa 30 anos de sua publicação. Por sua força e pelo que representa para todos nós. esteve cada vez mais presente e se tornou o mais conhecido e estudado poema de Gullar. Confirmando sua universalidade. \"Poema Sujo\" ainda ganhou traduções em Cuba. Colômbia. França. Espanha. Alemanha. Suécia e Estados Unidos",
+        "link": "https://fundbras.wordpress.com/wp-content/uploads/2013/04/ferreira-gullar-poema-sujo.pdf"
+      },
+    {
+        "nome": "O Corvo",
+        "categoria": "Poema Narrativo",
+        "imagem": "https://m.media-amazon.com/images/I/91GKn4g3fML._AC_UF1000,1000_QL80_.jpg",
+        "descricao": "\"A morte de uma mulher bela é, sem sombra de dúvida, o tema mais poético do mundo.\" Assim Edgar Allan Poe justificaria a gênese de O corvo, poema publicado sob pseudônimo originalmente em 1845. Mas o que faz com que esses versos hipnotizantes sobre perda e desejo, escritos de modo tão calculado pelo mestre do terror há quase dois séculos, tenham merecido tantos elogios e tamanha controvérsia?\nNesta edição, o leitor vai conhecer as traduções mais notáveis de \"O corvo\" para a nossa língua ― as de Fernando Pessoa e Machado de Assis ―, analisadas pelo poeta, tradutor e professor Paulo Henriques Britto, que também traduz três textos fundamentais de Poe sobre poesia (\"A filosofia da composição\", \"A razão do verso\" e \"O princípio poético\") e examina a faceta ensaística do escritor. ",
+        "link": "https://www.revistaprosaversoearte.com/o-corvo-edgar-allan-poe-traducao-fernando-pessoa/"
+      },
+    {
+        "nome": "O Cortiço",
+        "categoria": "Ficção Científica",
+        "imagem": "https://storage-aws-production.publica.la/bookwire-brazil/issues/2024/07/nOgn1RzjsY7m1eGQ/eee1877e-0b68-42de-a17f-c7e1e0f1532b_cover.jpg",
+        "descricao": "Descrição:O Cortiço, um dos romances mais importantes do Brasil no século 19, foi escrito por Aluísio Azevedo e é um incisivo retrato da formação da burguesia brasileira. A história gira em torno de João Romão, um português que vem ao Brasil com o sonho de enriquecer. Ele adquire um cortiço no Rio de Janeiro, um lugar insalubre e de difícil condição de vida, e também uma pedreira e uma venda. Seu plano de enriquecimento é sustentado pela exploração dos moradores do cortiço, que vivem em condições precárias.\n",
+        "link":"https://edisciplinas.usp.br/pluginfile.php/3669732/mod_resource/content/2/O%20Corti%C3%A7o.pdf"
+      },
+      {
+        "nome": "Apologia de Sócrates",
+        "categoria": "Filosofia",
+        "imagem": "https://m.media-amazon.com/images/I/91J2nXFBZIS._SL1500_.jpg",
+        "descricao": "A Apologia de Sócrates de Platão é o registro de uma das defesas mais famosas e polêmicas da história do Direito e da Justiça ocidentais. Paralelamente, trata-se de uma pequena obra-prima literária de um dos assistentes dessa defesa: Platão, um dos discípulos de Sócrates. O filósofo, que perpetuou conceitos como \"conheça-te a ti mesmo\" e \"só sei que nada sei\" foi condenado e passou dias na clausura, filosofando sobre a imortalidade da alma, antes de sua execução. Sócrates inicia seu discurso advertindo os juízes de que pronunciará exclusivamente a verdade. Assinada pelo jovem Meleto, Anito e Lícon, que, pelos costumes da época, tinham direito a fazer declaração jurada, a acusação indiciava Sócrates por não reconhecer os deuses que o Estado reconhecia, por introduzir novos cultos e, também, por corromper a juventude, motivos pelos quais receberia pena capital, caso fosse condenado. A tese defendida por Sócrates é a de que nada mais fazia do que filosofar. Inclusive, declarou que preferiria a morte a deixar de se dedicar à filosofia, e, infelizmente, foi o que aconteceu, uma vez que foi condenado por um júri composto de 501 homens atenienses. O pensador considerou vergonhosa a postura de seus julgadores pelo fato de terem sido persuadidos a acautelarem-se para não serem ludibriados pela sua “extraordinária capacidade de oratória”, que seus acusadores lhe atribuíram.",
+        "link": "https://edisciplinas.usp.br/pluginfile.php/270801/mod_resource/content/1/platao%20apologia%20de%20socrates.pdf"
+      },
+      
+    {
+        "nome": "Navio Negreiro",
+        "categoria": "Poemas",
+        "imagem": "https://m.media-amazon.com/images/I/71NMYymG6zL._AC_UF1000,1000_QL80_.jpg",
+        "descricao": "Com uma sensibilidade ímpar para a sua época, Castro Alves sabia que os tumbeiros vinham carregados de saberes ancestrais, carregados por povos que não eram sequer livres para morrer. Em sua condição de homem branco, Castro Alves volta-se contra a branquitude escravocrata e toma para si uma luta que ainda ecoa nos dias de hoje. .Além de ilustrações de Mulambö, projeto gráfico de Oga Mendonça e apresentação de Pétala e Isa Souza, do canal Afrofuturas, a edição tem posfácios do doutor em Teoria da Literatura e Literatura Comparada Luiz Henrique Oliveira (Cefet-MG), da atriz e poeta Elisa Lucinda, do escritor e jornalista Tom Farias e da historiadora e especialista na diáspora africana Mônica Lima e Souza (UFRJ)",
+        "link": "http://www.dominiopublico.gov.br/download/texto/bv000068.pdf"
+      },
+    {
+        "nome": "Canção do Exílio",
+        "categoria": "Poema Romântico",
+        "imagem": "https://www.arnaldoniskier.com.br/livraria/produtos/img/classicos_cancao_do_exilio.jpg",
+        "descricao": "\"Canção do Exílio\" é uma poesia romântica do escritor brasileiro Gonçalves Dias. A composição foi criada em julho de 1843, quando o autor se encontrava estudando direito na Universidade de Coimbra, em Portugal, ressaltando no poema o patriotismo e o saudosismo em relação à sua terra natal, o Brasil. ",
+        "link": "http://www.horizonte.unam.mx/brasil/gdias.html"
+      },
+    {
+        "nome": "Soneto de Fidelidade",
+        "categoria": "Poema Romântico",
+        "imagem": "https://m.media-amazon.com/images/I/41B0t1h788L._AC_UF1000,1000_QL80_.jpg",
+        "descricao": "O poema Soneto de Fidelidade é da autoria de Vinicius de Moraes e aborda os sentimentos de amor e fidelidade em um relacionamento.\nOs versos foram escritos no Estoril, em outubro de 1939, e foram posteriormente publicados no livro Poemas, Sonetos e Baladas (1946).",
+        "link":"https://www.viniciusdemoraes.com.br/pt-br/poesia/poesias-avulsas/soneto-de-fidelidade"
+      },
+    {
+        "nome": "Morte e Vida Severina",
+        "categoria": "Poemas",
+        "imagem": "https://m.media-amazon.com/images/I/81znk0mmeHL._SY385_.jpg",
+        "descricao": "Morte e Vida Severina (1954-55), poema que dá nome ao livro, é a obra mais popular de João Cabral e aborda o tema da seca nordestina, dando voz aos retirantes que fazem o duro percurso entre o rio Capibaribe e Recife.\nO poema O rio também retrata o universo árido às margens do rio Capibaribe, mas dá voz a ele próprio como condutor da narrativa. Engenhos de cana-de-açúcar, usinas, retirantes e trabalhadores são retratados na velocidade do correr das águas.\nEm Paisagens com figuras (1955), João Cabral sintetiza em palavras uma de suas principais características, que é o hibridismo de linguagens. Mesclando descrições de imagens de Pernambuco, com paisagens da Espanha, o poeta desfila toda sua expressividade onírica.\nPor fim, Uma faca sem lâmina (1955), trata do desafio da composição poética, que ele ilustra numa faca sem bainha, que corta o poeta por dentro.",
+        "link": "https://colegiocngparanagua.com.br/wp-content/uploads/2020/07/MORTE-E-VIDA-SEVERINA.pdf"
+      }
+]
+    const livro = infos.find(info => (info.nome == title));
+    var popup = document.getElementById('popup');
+    var popupImage = document.getElementById('popupImage');
+    var popupTitle = document.getElementById('popupTitle');
+    var popupSynopsis = document.getElementById('popupSynopsis');
+    var popupCategory = document.getElementById('popupCategory');
+    var popupBotao = document.getElementById('popupBotao');
+
+
+    popupImage.src = livro.imagem;
+    popupTitle.textContent = livro.nome;
+    popupSynopsis.textContent = livro.descricao;
+    popupCategory.textContent = livro.categoria;
+    popupBotao.href = livro.link;
+    popup.classList.add('abrirpopup');
+}
+
+function fecha() {
+    var popup = document.getElementById('popup');
+    popup.classList.remove('abrirpopup');
+}
+
+window.onclick = function(event) {
+    var popup = document.getElementById('popup');
+    if (event.target == popup) {
+        popup.classList.remove('abrirpopup');
+    }
+}
+
+
+
+
+// FILTROS
+var filtrar = document.querySelectorAll('.Novidades');
+var escritores = document.querySelectorAll('.escirotes')
+var livros = document.querySelectorAll('.livros')
+var poemas = document.querySelectorAll('.poema')
+var brasileiros = document.querySelectorAll('.brasileiros')
+var internacionais = document.querySelectorAll('.internacionais')
+var tendencia = document.querySelectorAll('.nTend')
+document.querySelector('#checknovidades').addEventListener("click", function(item){
+
+
+
+  var isChecked = document.getElementById("checknovidades").checked;
+  if (isChecked){
+    filtrar.forEach(item => item.style.display='none');
+    escritores.forEach(item => item.style.display='none');
+  }
+  else{
+    filtrar.forEach(item => item.style.display='');
+    escritores.forEach(item => item.style.display='')
+  }
+})
+
+
+
+
+
+
+document.querySelector('#checkpoemas').addEventListener("click", function(item2){
+  console.log(item2)
+
+  var isChecked = document.getElementById("checkpoemas").checked;
+  if (isChecked){
+    livros.forEach(item2 => item2.style.display='none');
+    escritores.forEach(item2 => item2.style.display='none');
+  }
+  else{
+    livros.forEach(item2 => item2.style.display='');
+    escritores.forEach(item2=> item2.style.display='')
+  }
+})
+
+
+document.querySelector('#checkfrases').addEventListener("click", function(item2){
+  console.log(item2)
+
+  var isChecked = document.getElementById("checkfrases").checked;
+  if (isChecked){
+    livros.forEach(item2 => item2.style.display='none');
+    poemas.forEach(item2 => item2.style.display='none');
+  }
+  else{
+    livros.forEach(item2 => item2.style.display='');
+    poemas.forEach(item2=> item2.style.display='')
+  }
+})
+
+document.querySelector('#checkbrasileiros').addEventListener("click", function(item2){
+  console.log(item2)
+
+  var isChecked = document.getElementById("checkbrasileiros").checked;
+  if (isChecked){
+    livros.forEach(item2 => item2.style.display='none');
+    poemas.forEach(item2 => item2.style.display='none');
+    internacionais.forEach(item2 => item2.style.display='none');
+  }
+  else{
+    livros.forEach(item2 => item2.style.display='');
+    poemas.forEach(item2=> item2.style.display='')
+    internacionais.forEach(item2 => item2.style.display='');
+  }
+})
+
+document.querySelector('#checkinternacionais').addEventListener("click", function(item2){
+  console.log(item2)
+
+  var isChecked = document.getElementById("checkinternacionais").checked;
+  if (isChecked){
+    livros.forEach(item2 => item2.style.display='none');
+    poemas.forEach(item2 => item2.style.display='none');
+    brasileiros.forEach(item2 => item2.style.display='none');
+  }
+  else{
+    livros.forEach(item2 => item2.style.display='');
+    poemas.forEach(item2=> item2.style.display='')
+    brasileiros.forEach(item2 => item2.style.display='');
+  }
+})
+
+
+document.querySelector('#checktendencia').addEventListener("click", function(item2){
+var isChecked = document.getElementById("checktendencia").checked;
+if (isChecked){
+  tendencia.forEach(item => item.style.display='none');
+  escritores.forEach(item2=> item2.style.display='none');
+}
+else{
+  filtrar.forEach(item => item.style.display='');
+  escritores.forEach(item2=> item2.style.display='')
+  tendencia.forEach(item => item.style.display='');
+
+}})
+
+
+
+
